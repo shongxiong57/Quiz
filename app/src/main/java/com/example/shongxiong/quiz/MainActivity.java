856034135107eb06ef4c3d64edd6eeb2c590a422
Shong Xiong <shongxiong57@gmail.com>
2017-07-14 00:53:44 -0700

    Added ImageView to background; implemented by adding a layout underneath

--- app/src/main/java/com/example/shongxiong/quiz/MainActivity.java
+++ app/src/main/java/com/example/shongxiong/quiz/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.shongxiong.quiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //final View MainView = (View) findViewById(R.id.parent);
+        //MainView.setBackgroundResource(R.drawable.ham);
+
+
+
     }
 }
